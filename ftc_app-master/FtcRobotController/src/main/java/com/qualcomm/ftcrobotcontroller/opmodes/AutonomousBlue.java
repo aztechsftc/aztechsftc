8fb26e4191154032f6529e6f45cd64ef6d6be124
rajgunda456 <rajendra.gunda@gmail.com>
2017-01-14 15:13:06 -0500

    working autonomous program

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousBlue.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousBlue.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by neeraja on 12/1/2016.
  */
@@ -20,12 +21,20 @@ public class AutonomousBlue extends LinearOpMode {
     DcMotor motorLaunchR;
 
 
-
     @Override
     public void runOpMode() throws InterruptedException {
 
-        // set up the hardware devices we are going to use
+        double powerFactor = MOTOR_POWER;
 
+        double nominalCoefficientRightMotor = 0.3; // go straight
+//        double nominalCoefficientRightMotor = 0.55; // go straight
+//        double nominalCoefficientRightMotor = 1;   //CURVES to the left
+        double nominalCoefficientLeftMotor = 1;
+
+        double motorPowerLeftNominal = nominalCoefficientLeftMotor * powerFactor;
+        double motorPowerRightNominal = nominalCoefficientRightMotor * powerFactor;
+
+        // set up the hardware devices we are going to use
         motorRight = hardwareMap.dcMotor.get("motorRight");
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         //motorLeft.setDirection(DcMotor.Direction.REVERSE);
@@ -34,7 +43,6 @@ public class AutonomousBlue extends LinearOpMode {
         motorBrush = hardwareMap.dcMotor.get("motorBrush");
         motorLaunchL = hardwareMap.dcMotor.get("motorLauncherL");
         motorLaunchR = hardwareMap.dcMotor.get("motorLauncherR");
-
 
 
         // wait for the start button to be pressed
@@ -50,15 +58,79 @@ public class AutonomousBlue extends LinearOpMode {
         //motorRight.setPower(-0.5 * MOTOR_POWER);
         //sleep(500);
 
-
         //comment
 
-        //goes forward and hits ball
-        //motorLeft.setPower(-MOTOR_POWER);
-        motorLeft.setPower(MOTOR_POWER);
-        motorRight.setPower(MOTOR_POWER*0.5);
-        sleep(750); //may need to changed, time in milliseconds
 
+        //execute the turn in 750sec
+        long startTime = System.currentTimeMillis();
+        long turningTime = 500;
+        double turnMagnitude = 0.4;  //0 means no turns 1 is max
+
+
+        motorLeft.setPower(1);
+        motorRight.setPower(.3);
+        sleep(500); //may need to changed, time in milliseconds
+
+
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        sleep(5000); //may need to changed, time in milliseconds
+
+
+        motorRight.setPower(-.55);
+        motorLeft.setPower(1.00);
+        sleep(200); //may need to changed, time in milliseconds
+
+        motorLeft.setPower(1);
+        motorRight.setPower(.55);
+        sleep(500); //may need to changed, time in milliseconds
+        //divide this turn into four segments
+
+        //segment 1 == start of turn
+//        long currentTime = System.currentTimeMillis();
+//        double elapsedTimeFraction = (currentTime - startTime) / turningTime;
+//        motorLeft.setPower(motorPowerLeftNominal * (1 + turnMagnitude * elapsedTimeFraction * (1 - elapsedTimeFraction)));
+//        motorRight.setPower(motorPowerRightNominal * (1 - turnMagnitude * elapsedTimeFraction * (1 - elapsedTimeFraction)));
+//        sleep(turningTime / 4); //may need to changed, time in milliseconds
+
+        //motorPowerLeftNominal*(1+turnMagnitude*elapsedTimeFraction*(1-elapsedTimeFraction))
+        //motorPowerRightNominal*(1-turnMagnitude*elapsedTimeFraction*(1-elapsedTimeFraction))
+
+        //segment 2 == into the turn
+//        currentTime = System.currentTimeMillis();
+//        elapsedTimeFraction = (currentTime - startTime) / turningTime;
+//        motorLeft.setPower(motorPowerLeftNominal * (1 + turnMagnitude * elapsedTimeFraction * (1 - elapsedTimeFraction)));
+//        motorRight.setPower(motorPowerRightNominal * (1 - turnMagnitude * elapsedTimeFraction * (1 - elapsedTimeFraction)));
+//        sleep(turningTime / 4); //may need to changed, time in milliseconds
+
+        //segment 3 == max turn
+//        currentTime = System.currentTimeMillis();
+//        elapsedTimeFraction = (currentTime - startTime) / turningTime;
+//        motorLeft.setPower(motorPowerLeftNominal * (1 + turnMagnitude * elapsedTimeFraction * (1 - elapsedTimeFraction)));
+//        motorRight.setPower(motorPowerRightNominal * (1 - turnMagnitude * elapsedTimeFraction * (1 - elapsedTimeFraction)));
+//        sleep(turningTime / 4); //may need to changed, time in milliseconds
+
+        //segment 4 == start straighten
+//        currentTime = System.currentTimeMillis();
+//        elapsedTimeFraction = (currentTime - startTime) / turningTime;
+//        motorLeft.setPower(motorPowerLeftNominal * (1 + turnMagnitude * elapsedTimeFraction * (1 - elapsedTimeFraction)));
+//        motorRight.setPower(motorPowerRightNominal * (1 - turnMagnitude * elapsedTimeFraction * (1 - elapsedTimeFraction)));
+//        sleep(turningTime / 4); //may need to changed, time in milliseconds
+
+        //segment 5 == straighten
+//        currentTime = System.currentTimeMillis();
+//        elapsedTimeFraction = (currentTime - startTime) / turningTime;
+//        motorLeft.setPower(motorPowerLeftNominal * (1 + turnMagnitude * elapsedTimeFraction * (1 - elapsedTimeFraction)));
+//        motorRight.setPower(motorPowerRightNominal * (1 - turnMagnitude * elapsedTimeFraction * (1 - elapsedTimeFraction)));
+//        sleep(turningTime / 4); //may need to changed, time in milliseconds
+
+        //Turn completed.  Now go in a straight line
+//        motorLeft.setPower(motorPowerLeftNominal);
+//        motorRight.setPower(motorPowerRightNominal);
+//        sleep(2000); //may need to changed, time in milliseconds
+
+
+        //stop the motors
         motorRight.setPower(0);
         motorLeft.setPower(0);
         sleep(5000);
@@ -68,17 +140,21 @@ public class AutonomousBlue extends LinearOpMode {
         //motorElevator.setPower(1);
         //sleep(6000);
 
-        motorLeft.setPower(MOTOR_POWER);
-        motorRight.setPower(MOTOR_POWER*0.5);
-        sleep(1000); //may need to changed, time in milliseconds
+        //motorLeft.setPower(MOTOR_POWER);
+        //motorRight.setPower(MOTOR_POWER * 0.5);
+        //sleep(1000); //may need to changed, time in milliseconds
 
-        motorRight.setPower(0);
-        motorLeft.setPower(0);
-        sleep(500);
+        //motorRight.setPower(0);
+        //motorLeft.setPower(0);
 
-        motorLeft.setPower(MOTOR_POWER);
-        motorRight.setPower(MOTOR_POWER*0.5);
-        sleep(500); //may need to changed, time in milliseconds
+
+
+
+        //sleep(500);
+
+        //motorLeft.setPower(MOTOR_POWER);
+        //motorRight.setPower(MOTOR_POWER * 0.5);
+        //sleep(500); //may need to changed, time in milliseconds
 //
 //        motorRight.setPowerFloat();  // if something doesn't work here, delete these 3 lines
 //        motorLeft.setPowerFloat();
@@ -90,7 +166,7 @@ public class AutonomousBlue extends LinearOpMode {
 //        sleep(500);  //may need to be changed
 
         // stop the motors
-        motorRight.setPower(0);
-        motorLeft.setPower(0);
+        //motorRight.setPower(0);
+        //motorLeft.setPower(0);
     }
 }
